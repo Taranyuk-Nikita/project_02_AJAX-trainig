d555202e3141c026f41dbd821a443463a2685978
Taranyuk Nikita <geargamer3112@gmail.com>
2021-04-07 23:13:18 +0800

    ФОФЧ_02

--- scripts/main_script.js
+++ scripts/main_script.js
@@ -4,11 +4,42 @@ window.addEventListener('DOMContentLoaded', () => {
     let result = document.querySelector('#result');
     let pageLoader = document.querySelector('#button');
 
-    button.addEventListener('click', () => {
+    pageLoader.addEventListener('click', () => {
         let promise = fetch('pages/ajax-01.html');
         
         promise
             .then(response => {return response.text()})
             .then(text => {result.innerHTML = text});
     });
+
+    /* Задача 3.2 для практики */
+    const insertText = document.querySelectorAll('.zadacha_3-2');   
+
+    insertText.forEach(but => {
+        but.addEventListener('click', () => {
+            let i = +but.textContent;
+            let promise = fetch(`pages/zadacha_3-2/${i}.html`);
+        
+            promise
+                .then(response => {return response.text()})
+                .then(text => {document.querySelector('.loadText').innerHTML = text});
+        });
+    });
+
+    /* Задача 3.5 для практики */
+    // const showTime = document.querySelector('#checkTime');
+
+    // showTime.addEventListener('click', () => {
+    //     let time = new Date();
+    //     document.querySelector('.timeSaver').innerHTML += `<li>${time}</li>`
+    // });
+
+    /* Задача 3.6 для практики */
+    const showTime = document.querySelector('#checkTime');
+        
+    setTimeout(function run(){
+        let time = new Date();
+        document.querySelector('.timeSaver').innerHTML += `<li>${time}</li>`;
+        setTimeout(run, 2000);
+    }, 2000)
 });
